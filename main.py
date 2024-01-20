8e2880023db54a0a1f26e6bf8de22633c5b1d7b4
Mucsanyi Balint <balint.mucsanyi@nokia.com>
2024-01-20 15:02:02 +0100

    letter in numerical order

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 def main():
-    pass
+    string = return_book("books/frankenstein.txt")
+    number_of_words = count_words(string)
+    print(number_of_words)
+    dict = count_letters(string)
+    report(dict)
 
 def return_book(path_to_book):
     with open(path_to_book) as f:
@@ -14,7 +18,32 @@ def count_words(book_as_string):
         word_counter += 1
     return word_counter
 
+def count_letters(book_as_string):
+    no_capitals = book_as_string.lower()
+    letters_dict = {}
+    for word in no_capitals:
+        for letter in word:
+            if letter not in letters_dict:
+                letters_dict[letter] = 0
+        letters_dict[letter] += 1
+    return letters_dict
+
+def sort_dict_alphanumerically(input_dict):
+    sorted_dict = {k: input_dict[k] for k in sorted(input_dict.keys())}
+    return sorted_dict
+
+def report(input_dict):
+    alpha_dict = {key: value for key, value in input_dict.items() if key.isalpha()}
+
+    alpha_list = sorted(alpha_dict.items(), key=lambda pair: pair[1], reverse=True)
+
+    for char, count in alpha_list:
+        print(f"The '{char}' character was found {count} times")
+    
+
 main()
-string = return_book("books/frankenstein.txt")
-number_of_words = count_words(string)
-print(number_of_words)
+
+
+#sorted_dict = sort_dict_alphanumerically(dict)
+#or key in sorted_dict:    
+ #   print(f"{key}: {dict[key]}")
